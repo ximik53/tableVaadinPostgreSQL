7ceda8fb3e079352f7faf316f124f6554d74a9ba
XiMiK <ximik53@yandex.ru>
2019-04-06 02:07:45 +0300

    Vaadin try

--- src/main/java/com/example/tableVaadinPostgreSQL/TableVaadinPostgreSqlApplication.java
+++ src/main/java/com/example/tableVaadinPostgreSQL/TableVaadinPostgreSqlApplication.java
@@ -2,9 +2,10 @@ package com.example.tableVaadinPostgreSQL;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TableVaadinPostgreSqlApplication {
+public class TableVaadinPostgreSqlApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TableVaadinPostgreSqlApplication.class, args);
